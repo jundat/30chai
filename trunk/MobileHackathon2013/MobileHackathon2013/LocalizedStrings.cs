fda6a7d34d1d69b3db963c5137b3b1d44b01a0a8
hailong.uit@gmail.com <hailong.uit@gmail.com@19ac25e3-4ef2-301b-9c8e-1092cecbe76c>
2013-11-15 06:08:22 +0000

    Blank project !

---
+++ trunk/MobileHackathon2013/MobileHackathon2013/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MobileHackathon2013.Resources;
+
+namespace MobileHackathon2013
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
